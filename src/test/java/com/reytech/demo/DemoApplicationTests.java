36f0307f4f67df32b62cf4c409bebf2307c526c6
jimmycos95 <73101327+jimmycos95@users.noreply.github.com>
2021-12-09 16:11:25 +0100

    Update DemoApplicationTests.java
    On change les tests

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -1,7 +1,11 @@
 package com.reytech.demo;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.reytech.demo.Personne;
+
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -9,5 +13,14 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+        void groupedAssertions() {
+        Personne pers = new Personne("Nassur", "Moumadi");
+
+        assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+                              () -> assertEquals("Moumadi", pers.getLastName()));
+    }
+
 
 }
